459f8697216d506bfbf17ac6b6d34745f8b04b41
ltaylr <ltaylr@protonmail.com>
2022-07-23 13:57:49 -0400

    OS, Events, callbacks notes

--- first-app/app.js
+++ first-app/app.js
@@ -1,7 +1,41 @@
+
+//use const to prevent overriding exports from logger.js
+const log = require('./logger.js');
+const path = require('path');
+const crypto = require('crypto');
+const os = require('os')
+const fs = require('fs');
+//log("ehllo");
+
 function sayHello(name)
 {
     console.log('hello ' + name);
 }
 
+var pathObj = path.parse(__filename);
+
+console.log(pathObj)
+
+var totMem = os.totalmem();
+var freeMem = os.freemem();
+
+console.log(`Total Memory: ${totMem}`)
+console.log(`Free Memory: ${freeMem}`)
 //sayHello('lawry');
-console.log(window);
+//console.log(window);
+
+const files = fs.readdirSync('./')
+
+console.log(files);
+
+fs.readdir('$', function(err, files)
+{
+    if (err) console.log('Error', err);
+
+    else console.log('Result', files);
+});
+
+
+
+
+
---
+++ first-app/logger.js
@@ -0,0 +1,13 @@
+
+var url = 'http://mylogger.io/log';
+
+function log(message)
+{
+    //send an http request
+
+    console.log(message);
+}
+
+module.exports = log;
+
+
